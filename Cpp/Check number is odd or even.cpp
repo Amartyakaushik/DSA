36c6f4b65204e093c18c81c2fe73e30d80c29ba1
Amartya kaushik <115655337+Amartyakaushik@users.noreply.github.com>
2023-10-30 19:14:01 +0530

    Add files via upload

--- Cpp/Check number is odd or even.cpp
+++ Cpp/Check number is odd or even.cpp
@@ -1,1 +1,15 @@
-asdf
+#include<iostream>
+using namespace std;
+
+//Check number is odd or even
+int main(){
+    int num;
+    cout<<"Enter ur number:";
+    cin>>num;
+    
+    if(num%2==0){
+        cout<<num<<" is an even number."<<endl;
+    }else {
+        cout<<num<<" is an odd number."<<endl;
+    }
+}
