0c5a50a298663945155a51073baca03cf76101eb
Amartya kaushik <115655337+Amartyakaushik@users.noreply.github.com>
2023-11-04 01:03:05 +0530

    Add files via upload

---
+++ Cpp/print inverted full pyramid.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+//print inverted full pyramid
+int main()
+{
+    int rowSize;
+    cout<<"Enter row of matrix: ";
+    cin>>rowSize;
+    
+    for(int row=0;row<rowSize;row++){
+        //Space
+        for(int col=0;col<row;col++){
+            cout<<" ";
+        }
+        //star
+        for(int col=0;col<rowSize-row;col++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
