2d559e9aabf41ae241617e81306f4838a924a089
Amartya kaushik <115655337+Amartyakaushik@users.noreply.github.com>
2024-01-11 20:07:18 +0530

    print all odd numbers between 1 to N and take N as input from the user.

---
+++ Java/PrintOdd.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class home{
+      public static void main(String [] args){
+            Scanner sc=new Scanner(System.in);
+            // Print odd numbers between 1 to N
+            int N=sc.nextInt();
+            for(int i=1;i<=N;i=i+2){
+                  System.out.println(i);
+            }
+
+      }
+}
