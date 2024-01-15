0c11015ffd27140ccb3c86c3b57ab4944283e65c
Amartya kaushik <115655337+Amartyakaushik@users.noreply.github.com>
2024-01-15 21:16:57 +0530

    Add files via upload

---
+++ Java/FullPyramid.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class FullPyramid {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the rowcount: ");
+        int rowcount=sc.nextInt();
+
+        for(int row=1; row<=rowcount; row++){
+            // for space
+            int space=rowcount-row;
+            for(int col=space; col>=1; col--){
+                System.out.print(" ");
+            }
+            //  for star
+            int star=(2*row)-1;
+            for(int col=0; col<star; col++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+//           * 
+//         * * *
+//       * * * * * 
+//     * * * * * * *
+//   * * * * * * * * *
