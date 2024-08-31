c42366ebec0235d3ba7b288bc344350d5be715f7
Amartya kaushik <115655337+Amartyakaushik@users.noreply.github.com>
2024-08-31 23:12:21 +0530

    Add files via upload

--- Cpp/ClassWork/Error.cpp
+++ Cpp/ClassWork/Error.cpp
@@ -73,3 +73,32 @@ int main(){
     }
     
 }
+
+
+#include<iostream>
+using namespace std;
+void insertionSort(int arr[], int size){
+    for(int i = 1; i< size ; i++){
+        int j = i -1;
+        int key = arr[i];
+        while(j >= 0 && arr[j] > key ){
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j+1] = key;
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i = 0; i<n; i++){
+        cin>>arr[i];
+    }
+    insertionSort(arr,n);
+    for(int i = 0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    
+}
