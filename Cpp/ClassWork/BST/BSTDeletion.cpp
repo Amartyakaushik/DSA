d34a1e03fa8959fd890f7fe9c0ba50066fc40d4e
Amartya kaushik <115655337+Amartyakaushik@users.noreply.github.com>
2024-11-09 23:27:55 +0530

    Add files via upload

--- Cpp/ClassWork/BST/BSTDeletion.cpp
+++ Cpp/ClassWork/BST/BSTDeletion.cpp
@@ -1,3 +1,6 @@
 #include<iostream>
 using namespace std;
 // level order traversal using queue
+// #include<iostream>
+// using namespace std;
+// // level order traversal using queue
--- Cpp/ClassWork/BST/BSTSInsertion.cpp
+++ Cpp/ClassWork/BST/BSTSInsertion.cpp
@@ -71,3 +71,76 @@ int main(){
         cout << "Element not found" << endl;
     }
 }
+// #include<iostream>
+// using namespace std;
+// struct Node{
+//     int data;
+//     Node* left;
+//     Node* right;
+
+//     Node(int val){
+//         data = val;
+//         left = right = nullptr;
+//     }
+// };
+
+// void inOrder(Node* root){
+//     // cout<<"Kaisa";
+//     if(root == nullptr){
+//         return;
+//     }
+//     inOrder(root -> left);
+//     cout<<root -> data << " ";
+//     inOrder(root -> right);
+// }
+
+// Node* searchBST(Node* root, int val){
+//     if(root == nullptr){
+//         return nullptr;
+//     }
+//     if(root -> data == val){
+//         return root;
+//     }else if(root -> data > val){
+//         return searchBST(root -> left, val);
+//     }else{
+//         return searchBST(root -> right, val);
+//     }
+// }
+
+
+// Node* insertBST(Node* root, int val){
+//     if(root == nullptr){
+//         return new Node(val);
+//     }
+
+//     if(val < root -> data){
+//         root -> left = insertBST(root -> left, val);
+//     }else{
+//         root -> right = insertBST(root -> right , val);
+//     }
+
+//     return root;
+// }
+
+
+// int main(){
+//     Node *root = nullptr;
+
+//     root = insertBST(root, 23);
+//     insertBST(root, 3);
+//     insertBST(root, 21);
+//     insertBST(root, 13);
+//     insertBST(root, 32);
+//     insertBST(root, 11);
+//     insertBST(root, 21);
+//     insertBST(root, 19);
+
+
+//     inOrder(root);
+
+//     if(searchBST(root,43) != nullptr){
+//         cout << "Element found" << endl;
+//     }else{
+//         cout << "Element not found" << endl;
+//     }
+// }
